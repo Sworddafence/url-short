31ba3a7c79cdc992cdc60f78b5ac082f2d32ebbf
Sworddafence <90822210+Sworddafence@users.noreply.github.com>
2025-02-04 16:19:39 -0800

    it works

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import zlib
 import sys
 import mysql.connector
@@ -28,22 +28,43 @@ def create_table(connection):
     finally:
         cursor.close()
 
-def query_for_hash(connection, hash):
+def query_for_hash(connection, hash, url):
     cursor = connection.cursor()
-    query = f"SELECT url FROM hash_urls WHERE hash = {hash};"
+
+    query = f"SELECT hash, url FROM hash_urls WHERE hash = '{hash}';"
+
     try:
         cursor.execute(query)
         results = cursor.fetchall()  # Fetch all rows
         print(results)
+        if(results == []):
+            query = f"INSERT INTO hash_urls (hash, url) VALUES ('{hash}', '{url}');"
+            print(query)
+            cursor.execute(query)
+            connection.commit()
+        else:
+            print("huh")
     except mysql.connector.Error as err:
         print(f"Error: {err}")
-    finally:
-        cursor.close()
+        return "failed"
     return results
 
 # Example usage
 
-
+def query_for_url(connection, hash):
+    cursor = connection.cursor()
+    query = f"SELECT url, hash FROM hash_urls WHERE hash = '{hash}';"
+    try:
+        cursor.execute(query)
+        results = cursor.fetchall()  # Fetch all rows
+        print(results)
+        print(type(results))
+        print(results[0][0])
+        return (results[0][0])
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+        return "failed"
+ 
 
 app = Flask(__name__)
 
@@ -66,17 +87,26 @@ def urlshortner():
     if request.method == 'POST':
         user_input = request.form['user_input']
         user_input = remove_http(user_input)
-        uers_input = user_input.encode('utf-8')
-        hash = zlib.crc32(uers_input)
+        user_input = user_input.lower()
+        url = user_input.encode('utf-8')
+        hash = zlib.crc32(url)
         hash = f"{hash & 0xFFFFFFFF:08x}"
         hash = hash[4:]
-        t = 0
-
-        #g = query_for_hash(connection, hash)
+        g = query_for_hash(connection, hash, user_input)
 
 
         return f'{cururl}/{hash}'
     return render_template('index.html')
+
+@app.route('/<path:randomstuff>')
+def catch_all(randomstuff):
+    # Redirect to a specific page, e.g., 'home'
+    g = query_for_url(connection, randomstuff)
+    #print(g[0])
+    ##return "hi"
+    url = f'https://{g}'
+    return redirect(url)
+
 
 # Run the application
 if __name__ == '__main__':
